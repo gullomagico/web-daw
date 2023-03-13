8d3bbdf2c4004c5fda1ea52b328611c52398b8dc
gullomagico <33353590+gullomagico@users.noreply.github.com>
2023-03-13 01:07:36 +0100

    Wip

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,10 +1,30 @@
-import classes from "./App.module.scss";
+import { AppBar, Toolbar, Typography, Button, IconButton, Box } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+// import classes from "./App.module.scss";
 
 function App() {
 
     return (
         <div className="App">
-            <h1>Home page</h1>
+            <Box sx={{ flexGrow: 1 }}>
+                <AppBar position="static" sx={{ height: 40 }}>
+                    <Toolbar>
+                        <IconButton
+                            size="large"
+                            edge="start"
+                            color="inherit"
+                            aria-label="menu"
+                            sx={{ mr: 2, maxHeight: 40 }}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="h6" component="div" sx={{ flexGrow: 1, maxHeight: 40 }}>
+                            News
+                        </Typography>
+                        <Button color="inherit">Login</Button>
+                    </Toolbar>
+                </AppBar>
+            </Box>
         </div>
     );
 }
--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -4,11 +4,14 @@ import {
     createBrowserRouter,
     RouterProvider,
 } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 import App from "./App";
 import Login from "./routes/login-page";
 import ProfilePage from "./routes/profile-page";
 import ErrorPage from "./routes/error-page";
+
+import "./global.scss";
 
 const router = createBrowserRouter([
     {
@@ -28,8 +31,23 @@ const router = createBrowserRouter([
     }
 ]);
 
+
+const theme = createTheme({
+    palette: {
+        mode: "dark",
+        primary: {
+            main: "#3f51b5",
+        },
+        secondary: {
+            main: "#f50057",
+        },
+    },
+});
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <ThemeProvider theme={theme}>
+            <RouterProvider router={router} />
+        </ThemeProvider>
     </React.StrictMode>
 );

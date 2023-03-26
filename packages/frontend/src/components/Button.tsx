c480011c315cc3c0a00f30ca265c5e73e7e17e80
gullomagico <33353590+gullomagico@users.noreply.github.com>
2023-03-26 17:05:08 +0200

    Add favicon

--- packages/frontend/src/components/Button.tsx
+++ packages/frontend/src/components/Button.tsx
@@ -1,6 +1,10 @@
-const Button = (props: any) => {
+interface AuxProps { 
+    children: React.ReactNode
+ }
+
+const Button = ({ children }: AuxProps) => {
     return (
-        <button className="text-gray-800 font-mono bg-gray-500 p-2 rounded-full w-24">{props.children}</button>
+        <button className="text-gray-800 font-mono bg-gray-500 p-2 rounded-full w-24">{children}</button>
     );
 };
 
